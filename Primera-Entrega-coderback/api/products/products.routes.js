ea5a5617109d29531c57d5927c0fa2c5f6f5c175
RDesign-Castex <113470690+RDesign-Castex@users.noreply.github.com>
2023-05-03 23:42:10 -0300

    codigo servidor express en prueba 1

--- Primera-Entrega-coderback/api/products/products.routes.js
+++ Primera-Entrega-coderback/api/products/products.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const productsController = require("./products.controller");
+const productsController = require("./products.controller.js");
+const upload = require("../../middleware/multerConfig.js");
 const router = express.Router();
 
 router.get("/", productsController.getProducts);
 router.get("/:pid", productsController.getProductById);
-router.post("/", productsController.createProduct);
+router.post("/", upload.single("thumbnail"), (req, res) => {});
 router.put("/:pid", productsController.updateProduct);
 router.delete("/:pid", productsController.deleteProduct);
 
